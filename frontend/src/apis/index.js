11840a96155ab9e6a7e79ae815124789b392702b
mitish13 <mitish1308@gmail.com>
2022-06-20 11:27:21 +0530

    delete frontend code

--- frontend/src/apis/index.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://codebookmitish13.herokuapp.com",
-});
--- frontend/src/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "./reducers/index.js";
-import thunk from "redux-thunk";
-
-const store = createStore(rootReducer, applyMiddleware(compose(thunk)));
-
-export default store;
